fbb5484423a768e27cb476d29af14932f8692648
wisdom1104 <jihye11050@gmail.com>
2023-06-07 19:36:34 +0900

    style: 상품등록 페이지 스타일

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useMutation, useQueryClient } from "react-query";
 import { addProduct } from "../api/shop";
+import styled from "styled-components";
 import { IoArrowBack } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
@@ -49,13 +50,13 @@ function Create() {
     navi("/");
   };
   return (
-    <div>
+    <Clounm>
       <form onSubmit={onSubmit}>
-        <div>
-          <div>
-            <button onClick={OnClickBackBtn}>
+        <Row>
+          <Contain>
+            <BackBtn onClick={OnClickBackBtn}>
               <IoArrowBack />
-            </button>
+            </BackBtn>
 
             <h1>이미지 업로드</h1>
 
@@ -69,57 +70,153 @@ function Create() {
                   src={selectedImage}
                   style={{ maxWidth: "300px", maxHeight: "300px" }}
                 />
-                <button
+                <Btn
                   onClick={() => {
                     setSelectedImage(null);
                   }}
                 >
                   X
-                </button>
+                </Btn>
               </div>
             ) : (
               <p>이미지를 선택해주세요.</p>
             )}
-          </div>
-          <div>
-            <div>
+          </Contain>
+          <Contain>
+            <Content>
               <h2>상품 정보</h2>
-              <div>
+              <Title>
                 Title:
-                <input
+                <Input
                   type="text"
                   value={title}
                   onChange={(e) => {
                     setTitle(e.target.value);
                   }}
                 />
-              </div>
-              <div>
+              </Title>
+              <Price>
                 Price:
-                <input
+                <Input
                   type="text"
                   value={price}
                   onChange={(e) => {
                     setPrice(e.target.value);
                   }}
                 />
-              </div>
-              <div>
+              </Price>
+              <Description>
                 Description:
-                <textarea
+                <Textarea
                   value={description}
                   onChange={(e) => {
                     setDescription(e.target.value);
                   }}
                 />
-              </div>
-              <button onSubmit={onSubmit}>업로드</button>
-            </div>
-          </div>
-        </div>
+              </Description>
+              <SubmitBtn onSubmit={onSubmit}>업로드</SubmitBtn>
+            </Content>
+          </Contain>
+        </Row>
       </form>
-    </div>
+    </Clounm>
   );
 }
 
 export default Create;
+const Textarea = styled.textarea`
+  width: 100%;
+  height: 250px;
+  font-size: 20px;
+`;
+const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  font-size: 20px;
+`;
+const Description = styled.div`
+  margin-top: 20px;
+  font-size: 15px;
+  line-height: 30px;
+  display: flex;
+  flex-direction: column;
+`;
+const Price = styled.div`
+  font-size: 20px;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 30px;
+  display: flex;
+  flex-direction: column;
+`;
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 85%;
+  height: 400px;
+  margin: auto;
+  margin-top: 20px;
+  padding: 30px 20px;
+  border-top: 1px solid lightgray;
+`;
+
+const BackBtn = styled.button`
+  background-color: transparent;
+  border: none;
+  font-size: 50px;
+  cursor: pointer;
+  &:hover {
+    color: steelblue;
+  }
+`;
+const SubmitBtn = styled.button`
+  background-color: white;
+  border: 1px solid gray;
+  border-radius: 10px;
+  font-size: 30px;
+  margin: 30px 80px;
+  cursor: pointer;
+  &:hover {
+    color: steelblue;
+    border: 2px solid steelblue;
+  }
+`;
+const Btn = styled.button`
+  background-color: transparent;
+  border: none;
+  /* position: absolute;
+  bottom: 20px;
+  left: 37%; */
+  &:hover {
+    color: steelblue;
+  }
+`;
+
+const Contain = styled.div`
+  width: 50%;
+  height: 800px;
+  position: relative;
+  padding: 20px;
+  /* background-color: steelblue; */
+`;
+
+const Clounm = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  min-width: 800px;
+  height: 97vh;
+  margin: auto;
+`;
+const Row = styled.div`
+  display: flex;
+  width: 90%;
+  margin: auto;
+`;
